031c2d558c4fc8d879196ec36871a3c33d914612
ThaiNguyen1710 <thai.nguyen.171002@gmail.com>
2023-12-06 11:49:23 +0700

    Updated view Product

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { Cart, FilterSection, Footer, Header, Home, HomeSlider } from "../components";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../api";
+import { getAllProducts, getAllUsers } from "../api";
 import { setAllProducts } from "../context/actions/productAction";
+import { setAllUserDetail } from "../context/actions/allUsersAction";
 
 const Main = () => {
   const products = useSelector((state) => state.products);
+  const allUsers = useSelector((state) => state.allUsers);
   const isCart = useSelector((state) => state.isCart);
   const dispatch = useDispatch();
 
@@ -13,6 +15,11 @@ const Main = () => {
     if (!products) {
       getAllProducts().then((data) => {
         dispatch(setAllProducts(data));
+      });
+    }
+    if (!allUsers) {
+      getAllUsers().then((data) => {
+        dispatch(setAllUserDetail(data));
       });
     }
   });
