f750eaabc13dc45a1fa4cfa5edcde1c4c425dd72
ThaiNguyen1710 <thai.nguyen.171002@gmail.com>
2024-06-28 00:58:00 +0700

    update Final DANTN :(

--- client/src/components/Admin/RevenueShipper.jsx
+++ client/src/components/Admin/RevenueShipper.jsx
@@ -106,7 +106,7 @@ const RevenueShipper = ({ shipperData, onClose }) => {
             >
               {/* <option value={2023}>2023</option> */}
               <option value={2024}>2024</option>
-              <option value={2024}>2025</option>
+              <option value={2025}>2025</option>
             </select>
             <label htmlFor="filterMonth" className="">
               Tháng:
